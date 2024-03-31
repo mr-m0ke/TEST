69e5eff849e79db772e7cc9e12cc8bbb85bd887a
mr-m0ke <ajglatczak@me.com>
2024-03-31 00:03:05 -0400

    Initial Commit

---
+++ TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by Andrew Glatczak on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
